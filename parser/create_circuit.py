81528a8dbd46f4aeb5cd1c86818c2a6e21962777
Simon <simon.bihlmaier@gmail.com>
2019-03-30 19:40:00 +0100

    split GateHelper off to extra file, added function for replacing not by nand

---
+++ parser/create_circuit.py
@@ -0,0 +1,37 @@
+from tools.gate import *
+from tools.person import Person
+from parser.gate_helper import GateHelper
+from parser.parse_native_format import parse_native
+
+
+def replace_not_by_nand(nonio_gate_list):
+    """
+    This will only work, if nonio_gate_list does not contain any nand gates
+
+    :param nonio_gate_list: list of gate_helper objects that are no input gates
+    :return: list of gates where all not-gates have been replaced by nand gates
+    """
+    for gate in nonio_gate_list:
+        if gate.type == "NOT":
+            gate.type = "NAND"
+            gate.num_of_inputs = 2
+
+        #duplicate all output_to entrys of ids of NOT and NAND Gates
+        add_to_output_to = []
+        for id in gate.output_to:
+            if nonio_gate_list[id-1].type == "NOT" or nonio_gate_list[id-1].type == "NAND":
+                add_to_output_to.append(id)
+        gate.output_to += add_to_output_to
+
+    return nonio_gate_list
+
+
+def create_circuit_from_output_data(output_file, person: Person):
+
+    input_gate_list, rangeA, rangeB, nonio_gate_list = parse_native(output_file)
+
+    replace_not_by_nand(nonio_gate_list)
+
+    print(nonio_gate_list)
+
+create_circuit_from_output_data("parser/gate_files", None)

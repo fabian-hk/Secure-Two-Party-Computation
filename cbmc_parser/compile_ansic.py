5c80246ce50379a429b793857422b37dc6ec8453
Simon <simon.bihlmaier@gmail.com>
2019-05-06 17:25:19 +0200

    documentation for compile_ansic

--- cbmc_parser/compile_ansic.py
+++ cbmc_parser/compile_ansic.py
@@ -6,6 +6,12 @@ import conf
 # This will only work if CBMC-GC is linked in the conf.py
 
 def cbmc_gc_compile(ansic_file_path):
+    """
+    Function that will call CBMC-GC for compiling a .c file and create output.* files representing the circuit
+
+    :param ansic_file_path: name of the ANSI-C .c file that is compiled
+    :return: nothing is returned - results are saved to cbmc_parser/gate_files/default_output
+    """
     if not os.path.isdir(conf.default_output):
         os.mkdir(conf.default_output)
     goto_location_for_output = "cd " + conf.default_output + "; "
